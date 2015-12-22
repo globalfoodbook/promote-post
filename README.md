a47e8711ee5776be18b82f4af536c9559ce2f7b0
Ikenna Okpala <me@ikennaokpala.com>
2015-12-22 23:58:17 +0000

    Modify read me

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# promote-post
+# Promote Posts Plugin by Global Food Book
 WIP GFB's Promote Post plugin..
 
+This plugin assists with delivery and promotion of post on social networks.
--- promote-posts.php
+++ promote-posts.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: Promote Posts Plugin
 Plugin URI: http://wordpress.org/extend/plugins/gfb-promote-post/
-Description: This plugin is an extract from <a href='http://globalfoodbook.com' target='_blank'>globalfoodbook.com</a>. This assits with delivery and promotion of post on social networks.
+Description: This plugin is an extract from <a href='http://globalfoodbook.com' target='_blank'>globalfoodbook.com</a>. This assists with delivery and promotion of post on social networks.
 Author: Ikenna N. Okpala
 Version: 1.0
 Author URI: http://ikennaokpala.com/
