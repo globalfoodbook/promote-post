a729eace91084a90a572a7a6797a1fea69aa4e53
Ikenna Okpala <me@ikennaokpala.com>
2015-12-22 23:56:31 +0000

    Add plugin description

---
+++ promote-posts.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Promote Posts Plugin by Global Food Book
+ * @version 1.0
+ */
+/*
+Plugin Name: Promote Posts Plugin
+Plugin URI: http://wordpress.org/extend/plugins/gfb-promote-post/
+Description: This plugin is an extract from <a href='http://globalfoodbook.com' target='_blank'>globalfoodbook.com</a>. This assits with delivery and promotion of post on social networks.
+Author: Ikenna N. Okpala
+Version: 1.0
+Author URI: http://ikennaokpala.com/
+*/
